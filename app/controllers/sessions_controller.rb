9f9fd3fe299949e7273349a14701d328e38c421b
dwilches13 <daniel.wilches@kellogg.northwestern.edu>
2022-02-22 17:31:52 +0000

    1st Commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,37 @@
+class SessionsController < ApplicationController
+
+    def new
+        session[:user_id]
+    end
+
+    def create
+        entered_email = params["email"]
+        entered_password = params["password"]
+        @user = User.find_by({email: entered_email})
+            # checking the email
+        if @user # yes, email matches
+            # checking the password
+            if BCrypt::Password.new(@user.password) == entered_password
+                # the password matches!
+                session["user_id"] = @user.id
+                flash[:notice] = "Welcome #{@user.first_name}!"
+                redirect_to "/companies"
+            else
+                # password doesn't math
+                flash[:notice] = "Password is incorrect"
+                redirect_to "/sessions/new"
+            end
+        else
+            # email doesn't match, send back to the login page
+            flash[:notice] = "No user found"
+            redirect_to "/sessions/new"
+        end
+    end
+
+    def destroy
+        session["user_id"] = nil
+        flash[:notice] = "You have been logged out!"
+        redirect_to "/sessions/new"
+    end
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+#{"first_name"=>"Daniel",
+# "last_name"=>"Wilches Giraldo",
+# "email"=>"daniel.wilches@kellogg.northwestern.edu",
+# "password"=>"tacos"},
+# "commit"=>"Sign up"}
+
+
   def create
+    plain_text_password = params["user"]["password"] #=> "tacos"
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(plain_text_password)
     @user.save
     redirect_to "/users/#{@user.id}"
   end
